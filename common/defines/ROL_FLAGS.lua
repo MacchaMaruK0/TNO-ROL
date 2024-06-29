0343e0c8d84d4a5edf9b9d3b29b75297478bc379
Sorrowful men <112162283+Sorrowfulmen@users.noreply.github.com>
2024-06-29 16:13:27 +0800

    移植杂项法律

--- common/defines/ROL_FLAGS.lua
+++
@@ -1,3 +0,0 @@
-NDefines_Graphics.NGraphics.COUNTRY_FLAG_SMALL_TEX_MAX_SIZE = 512
-NDefines_Graphics.NGraphics.COUNTRY_FLAG_MEDIUM_TEX_MAX_SIZE = 512
-NDefines_Graphics.NGraphics.COUNTRY_FLAG_TEX_MAX_SIZE = 512
